af504c2500587a7a9fcacbcb8b07431f868422a1
John Kim <joki9448@gmail.com>
2022-05-31 16:22:30 -0400

    throws error when incorrect login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,17 +6,22 @@ const Login = ({ username, setUsername, password, setPassword }) => {
           headers: {"Content-Type": "application/json"},
           body: JSON.stringify({ username, password })
         })
+        let res = await req.json()
+        if (req.status === 201) {
+          alert('Login successful! HONK')
+        }
+        else {
+          alert(res.error)
+          // console.log(res.error)
+        }
     }
     
+
     return (
         <form className="login" action="/login" method="POST" onSubmit={(e) => {
             e.preventDefault();
             handleLogin()
             }}>
-            {/* <input type="text" name="user[username]"></input>
-            <input type="password" name="user[password]"></input>
-            <input type="password" name="user[password_confirmation]"></input>
-            <input type="submit"></input> */}
             <input type="text" onChange={(e) => setUsername(e.target.value)} value={username} placeholder="Username"></input>
             <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="Password"></input>
             <input type="submit"></input>
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -13,10 +13,6 @@ const Signup = ({ username, setUsername, email, setEmail, password, setPassword,
             e.preventDefault();
             handleSignUp()
             }}>
-            {/* <input type="text" name="user[username]"></input>
-            <input type="password" name="user[password]"></input>
-            <input type="password" name="user[password_confirmation]"></input>
-            <input type="submit"></input> */}
             <input type="text" onChange={(e) => setUsername(e.target.value)} value={username} placeholder="Username"></input>
             <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} placeholder="Email"></input>
             <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="Password"></input>
