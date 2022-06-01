2446373b52191a7f4eb8ac166932c2e413639142
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-01 14:13:03 -0400

    Seeds created and tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating  users'
+u1 = User.create(username: "user1", email: "user1@example.com", password:"123", password_confirmation:"123")
+u2 = User.create(username: "user2", email: "user2@example.com", password:"1234", password_confirmation:"1234")
+u3 = User.create(username: "user3", email: "user3@example.com", password:"12345", password_confirmation:"12345")
+u4 = User.create(username: "user4", email: "user4@example.com", password:"123456", password_confirmation:"123456")
+u5 = User.create(username: "user5", email: "user5@example.com", password:"1234567", password_confirmation:"1234567")
+
+
+puts 'Creating Clubs'
+c1 = Club.create(name: "Goose's Digest", description:"All genres welcome")
+c2 = Club.create(name: "Banned Books For Rebel Geese", description: "First rule of Banned Books For Rebel Geese, never talk about Banned Books For Rebel Geese")
+
+puts 'Creating Memberships'
+m1 = Membership.create(user_id: u1.id, club_id: c1.id, admin: true)
+m2 = Membership.create(user_id: u2.id, club_id: c2.id, admin: true)
+m3 = Membership.create(user_id: u3.id, club_id: c1.id, admin: false)
+m4 = Membership.create(user_id: u4.id, club_id: c1.id, admin: false)
+m5 = Membership.create(user_id: u5.id, club_id: c1.id, admin: false)
+m6 = Membership.create(user_id: u5.id, club_id: c2.id, admin: false)
+m7 = Membership.create(user_id: u3.id, club_id: c2.id, admin: false)
+puts 'Seeding done'
