578aaa5db19ff2d0cdc36c4043cb403eeb13a6d4
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-01 16:24:32 -0400

    new club plus membership created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :clubs
+  resources :clubs, only: [:index, :create, :destroy]
   resources :memberships
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   
   get '/me', to: 'users#show'
 
-  get '/clubs', to: 'clubs#index'
-  post '/clubs', to: 'clubs#create'
-  delete '/clubs/:id', to: 'clubs#destroy'
+  # get '/clubs', to: 'clubs#index'
+  # post '/clubs', to: 'clubs#create'
+  # delete '/clubs/:id', to: 'clubs#destroy'
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
