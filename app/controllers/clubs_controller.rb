25003c82c10b7ee6071aac258e5eb5221a0fc9c6
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-01 15:46:46 -0400

    started club actions

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -3,4 +3,21 @@ class ClubsController < ApplicationController
     clubs = Club.all
     render json: clubs
   end
+
+  def create
+    club = Club.create!(club_params)
+    render json: club
+  end
+
+  def destroy
+    club = Club.find_by(id: params[:id])
+    #destroy dependents
+    head :no_content
+  end
+
+  private
+  def club_params
+    params.permit(:name, :description)
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/me', to: 'users#show'
 
   get '/clubs', to: 'clubs#index'
+  post '/clubs', to: 'clubs#create'
+  delete '/clubs/:id', to: 'clubs#destroy'
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
