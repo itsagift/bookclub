5b75bcbe06b703a02a1f606fbd643c56159a1992
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-02 17:04:16 -0400

    Link styling basic

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -10,30 +10,10 @@ function NavBar({setUser, user}) {
         setUser(null)
     }
 
-    const linkStyles = {
-        // display: "inline-block",
-        // width: "50px",
-        // padding: "12px",
-        // margin: "0 6px 6px",
-        // background: "blue",
-        // textDecoration: "none",
-        // color: "white",
-        ///
-        fontFamily: 'Patrick Hand',
-        fontSize: "1.25rem",
-        background: "black",
-        color: "white",
-        borderRadius: "20px",
-        padding: "5px",
-        marginTop: "10px",
-        textDecoration: "none"
-
-      };
-
     if (!user) {
-        return <span><NavLink to="/" exact style={linkStyles}> Home </NavLink></span>
+        return <span><NavLink to="/" exact><button className="login-button">Home</button></NavLink></span>
         }
-      return <span><NavLink to="/" exact style={linkStyles}> Home </NavLink>
+      return <span><NavLink to="/" exact><button className="login-button">Home</button></NavLink>
       <button className="login-button" onClick={()=> handleLogout()}>Logout</button></span>
         
 }
