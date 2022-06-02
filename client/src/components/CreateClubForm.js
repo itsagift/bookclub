b3327078d647d1781dd48f96be33cace14cf5e9e
John Kim <joki9448@gmail.com>
2022-06-02 14:57:01 -0400

    styled form

--- client/src/components/CreateClubForm.js
+++ client/src/components/CreateClubForm.js
@@ -3,9 +3,9 @@ function CreateClubForm({ formVisible, setFormVisible }){
         <div className="club-form-container" style={{display: formVisible ? "block" : "none"}}>
             <a className="close-button" href="#" onClick={() => setFormVisible(false)}>x</a>
             <form className="create-club-form">
-                <input type="text" placeholder="Club Name"></input>
-                <input type="text" placeholder="Describe your club"></input>
-                <input type="submit"></input>
+                <input type="text" className="create-club-input-text" placeholder="Name your club..."></input>
+                <textarea type="text" className="create-club-input-text" placeholder="Describe your club..."></textarea>
+                <input type="submit" className="form-button"></input>
             </form>
         </div>
     )
