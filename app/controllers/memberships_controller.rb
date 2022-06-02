499b077746a4db712b27bd3661a2398b7fc8f770
Gautam Sinha <gautam.sinha@live.com>
2022-06-02 18:07:15 -0400

    backend error for existing user

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -8,6 +8,11 @@ class MembershipsController < ApplicationController
 
   def create 
     membership = Membership.create!(membership_params)
+   if membership.valid? 
+    render json: membership 
+   else
+     render json: {"error": membership.errors.full_messages}, status: :unprocessable_entity
+   end
     render json: membership, status: :created
   end
 
