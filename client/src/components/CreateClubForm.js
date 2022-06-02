f5b6a69fe86711367cc8aeb69fbab157124fa127
John Kim <joki9448@gmail.com>
2022-06-02 16:51:21 -0400

    finished base form frontend

--- client/src/components/CreateClubForm.js
+++ client/src/components/CreateClubForm.js
@@ -2,9 +2,10 @@ function CreateClubForm({ formVisible, setFormVisible }){
     return (
         <div className="club-form-container" style={{display: formVisible ? "block" : "none"}}>
             <a className="close-button" href="#" onClick={() => setFormVisible(false)}>x</a>
+            <h1 style={{fontFamily: "Patrick Hand", fontSize: "40px", textAlign: "center", marginTop: "6%"}}>Start a Bookclub</h1>
             <form className="create-club-form">
                 <input type="text" className="create-club-input-text" placeholder="Name your club..."></input>
-                <textarea type="text" className="create-club-input-text" placeholder="Describe your club..."></textarea>
+                <textarea type="text" rows="8" className="create-club-input-text" placeholder="Describe your club..."></textarea>
                 <input type="submit" className="form-button"></input>
             </form>
         </div>
