97fb744fdc450fcda7544b8d01e9e630dc785ca1
Elizabeth Dowling <dowlinge94@gmail.com>
2022-06-02 16:51:20 -0400

    linkStyles used instead of CSS so as not to intefere

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -9,10 +9,31 @@ function NavBar({setUser, user}) {
         })
         setUser(null)
     }
+
+    const linkStyles = {
+        // display: "inline-block",
+        // width: "50px",
+        // padding: "12px",
+        // margin: "0 6px 6px",
+        // background: "blue",
+        // textDecoration: "none",
+        // color: "white",
+        ///
+        fontFamily: 'Patrick Hand',
+        fontSize: "1.25rem",
+        background: "black",
+        color: "white",
+        borderRadius: "20px",
+        padding: "5px",
+        marginTop: "10px",
+        textDecoration: "none"
+
+      };
+
     if (!user) {
-        return <NavLink to="/" exact> Home </NavLink>
+        return <span><NavLink to="/" exact style={linkStyles}> Home </NavLink></span>
         }
-      return <span><NavLink to="/" exact> Home </NavLink>
+      return <span><NavLink to="/" exact style={linkStyles}> Home </NavLink>
       <button className="login-button" onClick={()=> handleLogout()}>Logout</button></span>
         
 }
