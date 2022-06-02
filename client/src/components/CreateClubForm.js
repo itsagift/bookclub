3a784dad1400628f388343fa86486e158681c2ac
John Kim <joki9448@gmail.com>
2022-06-02 14:38:33 -0400

    started create club form

--- client/src/components/CreateClubForm.js
+++ client/src/components/CreateClubForm.js
@@ -2,6 +2,11 @@ function CreateClubForm({ formVisible, setFormVisible }){
     return (
         <div className="club-form-container" style={{display: formVisible ? "block" : "none"}}>
             <a className="close-button" href="#" onClick={() => setFormVisible(false)}>x</a>
+            <form className="create-club-form">
+                <input type="text" placeholder="Club Name"></input>
+                <input type="text" placeholder="Describe your club"></input>
+                <input type="submit"></input>
+            </form>
         </div>
     )
 }
