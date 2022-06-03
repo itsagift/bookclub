ca52fb5ed65738f509f2677ca8df0f4ddbef4070
Gautam Sinha <gautam.sinha@live.com>
2022-06-02 21:00:13 -0400

    fixed password confirmation

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -23,7 +23,7 @@ function SignupForm ({setUser}) {
             <input type="text" className="input-text" name="username" placeholder="Username"></input>
             <input type="email" className="input-text" name="email" placeholder="Email"></input>
             <input type="password" className="input-text" name="password" placeholder="Password"></input>
-            <input type="password" className="input-text" name="passwordconfirmation" placeholder="Confirm Password"></input>
+            <input type="password" className="input-text" name="password_confirmation" placeholder="Confirm Password"></input>
             <input type="submit" className='form-button' value="Sign Up"></input>
           </form>
     )
